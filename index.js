026d180f62af8191633b196e0002283d8c20f1af
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-09 16:48:00 -0700

    all test pass

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const { isNumber } = require('./lib/types.js');
+const { isNumber,
+        isBoolean,
+        isObject,
+        isArray,
+        isFunction,
+        isString
+
+ } = require('./lib/types.js');
 
 console.log(isNumber('3'));
+console.log(isBoolean(true));
+console.log(isObject({}));
+console.log(isArray([]));
+console.log(isFunction(3));
+console.log(isString('hi'));
+
 
