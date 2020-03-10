dc769414e651674a2fd943ef548d60aaab5020d6
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-10 16:12:18 -0700

    some progress

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const { isNumber,
-        isBoolean,
-        isObject,
-        isArray,
-        isFunction,
-        isString
+const {nameValidator,
+  colorValidator,
+  milesWalkedValidator, } = require('./lib/Validator.js');
 
- } = require('./lib/types.js');
 
-console.log(isNumber('3'));
-console.log(isBoolean(true));
-console.log(isObject({}));
-console.log(isArray([]));
-console.log(isFunction(3));
-console.log(isString('hi'));
+const dog = {
+  name: 'Seger',
+  color: 'brindle',
+  distance: '69',
+};
+
+console.log(nameValidator.validate(dog));
+console.log(colorValidator.validate(dog));
+console.log(milesWalkedValidator.validate(dog));
+
+
+// const { isNumber,
+//         isBoolean,
+//         isObject,
+//         isArray,
+//         isFunction,
+//         isString
+
+//  } = require('./lib/types.js');
+
+// console.log(isNumber('3'));
+// console.log(isBoolean(true));
+// console.log(isObject({}));
+// console.log(isArray([]));
+// console.log(isFunction(3));
+// console.log(isString('hi'));
 
 
